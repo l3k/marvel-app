88593e39022bb112b00d24dc7a003b5d5ceb5db2
Lucas Krul <l3k.scorp@gmail.com>
2022-04-13 20:55:19 -0300

    Create structure, config libs and global styles

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import {
+  useFonts,
+  RobotoCondensed_400Regular,
+  RobotoCondensed_700Bold,
+} from '@expo-google-fonts/roboto-condensed';
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    RobotoCondensed_400Regular,
+    RobotoCondensed_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+export default {
+  colors: {
+    title: '#000000',
+    header: '#393939',
+    background: '#4A4A4A',
+    red: '#EC1D24',
+    card: '#FFFFFF',
+  },
+
+  fonts: {
+    regular: 'RobotoCondensed_400Regular',
+    bold: 'RobotoCondensed_700Bold',
+  },
+};
